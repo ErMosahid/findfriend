271900ce4fbc1c02e14787c24eb1e4c822176329
ErMosahid <92686210+ErMosahid@users.noreply.github.com>
2021-10-18 09:35:06 +0545

    Add files via upload

---
+++ templete/footer.php
@@ -0,0 +1,17 @@
+
+		
+		
+		
+		<div id="footer">
+			<h2>&copy;www.mosahid.com.np</h2>
+		</div>
+		
+		
+		</div>
+		<!--Container ends here-->
+		
+		
+
+
+</body>
+</html>
---
+++ templete/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> find friends</title>
+	<link rel="stylesheet" href="styles/style.css" media="all"/>
+</head>
+<body>
+	<!--Container starts here-->
+	<div class="container">
+	
+		<!--header wrapper starts here-->
+		<div id="header_wrapper">
+		
+		
+			<!--header starts here-->
+			<div id="header">
+			
+				<img src="images/flogo.jpg" style="float:left"/>
+				
+				<form method="post" action=" "id="form1">
+					<strong>Email:</strong>
+					<input type="email" name="email" placeholder="Email"required="required"/>
+					<strong>Password</strong>
+					<input type="password" name="pass" placeholder="*****"required="required"/>
+					<button name="login">Login</button>
+					
+				</form>
+			
+			</div>
+			<!--header ends here-->
+		</div>
+		<!--header wrapper ends here-->
